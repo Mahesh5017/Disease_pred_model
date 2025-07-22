88af309eb9257e59b1a35ad58f22b7ff8fb33477
Mahesh5017 <nmahesh0802@gmail.com>
2025-07-22 20:31:46 +0530

    Fix: updated requirements and model path

--- test.py
+++
@@ -1,9 +0,0 @@
-from scripts.inference import predict_image
-
-# result = predict_image("D:/DISEASE_PRED/dataset/PlantVillage/Potato___healthy/0be9d721-82f5-42c3-b535-7494afe01dbe___RS_HL 1814.JPG")
-# result = predict_image("D:/DISEASE_PRED/dataset/PlantVillage/Tomato__Tomato_YellowLeaf__Curl_Virus/0a0fe942-bb9e-4384-8466-779017d00bcf___UF.GRC_YLCV_Lab 02192.JPG")
-
-# result = predict_image("D:/DISEASE_PRED/dataset/PlantVillage/Tomato__Tomato_YellowLeaf__Curl_Virus/0a0fe942-bb9e-4384-8466-779017d00bcf___UF.GRC_YLCV_Lab 02192.JPG")
-result = predict_image("D:/DISEASE_PRED/dataset/PlantVillage/Potato___Early_blight/0a47f32c-1724-4c8d-bfe4-986cedd3587b___RS_Early.B 8001.JPG")
-
-print(result)
